2ea6722340053883dfbc72561715e9de5dc78338
4n4l1st-chr4l3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-02-16 06:05:16 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char *s = "Programming is like building a multilingual puzzle \n";
+
+	puts(s);
+
+	return (0);
+}
