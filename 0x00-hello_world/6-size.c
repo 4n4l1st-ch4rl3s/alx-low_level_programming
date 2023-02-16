31c066f16938c21400fcc39dae33c14fcd398a22
4n4l1st-chr4l3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-02-16 07:32:24 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+        printf("Size of a char: %lu byte(s)", sizeof(char));
+        printf("\nSize of an int: %lu byte(s)", sizeof(int));
+        printf("\nSize of a long int: %lu byte(s)", sizeof(long));
+        printf("\nSize of long long int: %lu byte(s)", sizeof(long long));
+        printf("\nSize of a float: %lu byte(s)", sizeof(float));
+
+                return 0;
+
+}
