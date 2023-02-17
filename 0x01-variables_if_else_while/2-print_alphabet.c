a7d63b9d58c3e1e70e2497f52ba7ab879842328b
4n4l1st-chr4l3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-02-17 10:33:27 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *  Return: 0
+ */
+
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+
+	{
+
+		putchar(x);
+		putchar('\n')
+
+	}
+
+	return (0);
+}
