9430c7651e1afe8b76e0f93544f5b03f15c7ee7e
4n4l1st-chr4l3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-02-17 11:37:03 -0800

    very correct

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 	char msg[] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for(i = 0; i < 24; i++)
+	for (i = 0; i < 24; i++)
 	{
 		putchar(msg[i]);
 	}
 
 	putchar('\n');
-	return(0);
+	return (0);
 }
