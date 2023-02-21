73876ef405a6c674e47b69f96ebabdef05f87204
4n4l1st-chr4l3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-02-21 14:03:13 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	unsigned long fib1 = 0, fib2 = 1, y;
+
+	for (x =0; x < 50; x++)
+	{
+		y = fib1 +fib2;
+		printf("%lu", y);
+
+		fib1 = fib2;
+		fib2 = y;
+
+		if (x == 49)
+			print("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
