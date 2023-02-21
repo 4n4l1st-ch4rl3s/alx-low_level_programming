88da152ebbaee84723a1ab1bd597607f975c63ff
4n4l1st-chr4l3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-02-21 10:43:31 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  print_last_digit - Entry Point prints last digit
+ *  @x: input
+ *  Return: result
+ */
+
+int print_last_digit(int x)
+{
+	result = x % 10;
+	return (result);
+}
