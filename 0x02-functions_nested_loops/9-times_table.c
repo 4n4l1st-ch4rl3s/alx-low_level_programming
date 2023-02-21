437b2034c277dfc4507cc7aa8ca42bcfd2d41796
4n4l1st-chr4l3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-02-21 11:48:34 -0800

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ *  times_table - prints the 9 times table
+ *  Return: table
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+			if ( c <= 9)
+			{
+				if (b != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
