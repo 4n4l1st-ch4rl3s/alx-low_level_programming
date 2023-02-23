c3f6ced878a3b04b743723090c7f44dcd2a3b499
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-02-23 15:50:54 +0300

    multiply

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,22 +4,10 @@
  * mul - multiples teo numbers
  * @a: var 1
  * @b: var 2
- * Return: 0
+ * Return:  a * b
  */
 
 int mul(int a, int b)
 {
-	int result;
-
-	result = a * b;
-	if (result < 0)
-	{
-		_putchar('-');
-		result = -result;
-	}
-
-	if (result / 10)
-		print(result / 10);
-
-	_putchar(result % 10 + '0');
+	return (a * b);
 }
