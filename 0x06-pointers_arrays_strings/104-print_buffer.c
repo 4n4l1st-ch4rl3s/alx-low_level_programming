84f5234cc63d8ff45c95bb05b8c0a7859a9db342
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-03-02 18:00:29 +0300

    buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int a, x, d;
+
+	a = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (a < size)
+	{
+		x = size - a < 10 ? size - a : 10;
+		printf("%08x: ", a);
+		for (d = 0; d < 10; d++)
+		{
+			if (d < x)
+				printf("%02x", *(b + a + d));
+			else
+				printf("  ");
+			if (d % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (d = 0; d < x; d++)
+		{
+			int c = *(b + a + d);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		a += 10;
+	}
+}
