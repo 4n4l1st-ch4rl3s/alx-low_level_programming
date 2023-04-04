971935d9d9664c3f65ae2cb52fb7c2c405d533d1
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-04-04 14:34:52 +0300

    strchr exercise

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: variable 1
+ * @c: variable 2
+ * Return: string
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == '\0')
+			return (s + 1);
+
+		s++;
+	}
+	return (s + 1);
+}
