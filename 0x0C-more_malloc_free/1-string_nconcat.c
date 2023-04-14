c8a7ddadbdb858babbc57293f6bc2490fbac987a
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-04-14 04:05:48 +0300

    1-string_nconcat correct

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,23 +12,27 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len, s2_len;
+	unsigned int s1_len = n, x;
 	char *result;
 
-	s1 = (s1 != NULL) ? s1 : "";
-	s2 = (s2 != NULL) ? s2 : "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (x = 0; s1[x]; x++)
+		s1_len++;
 
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
-
-	if (n >= s2_len)
-		n = s2_len;
-	result = malloc(sizeof(char) * (s1_len * n + 1));
+	result = malloc(sizeof(char) * (s1_len + 1));
 
 	if (result == NULL)
 		return (NULL);
-	strcpy(result, s1);
-	strncat(result, s2, n);
-	result[s1_len + n] = '\0';
+	s1_len = 0;
+
+	for (x = 0; s1[x]; x++)
+		result[s1_len++] = s1[x];
+	for (x = 0; s2[x] && x < n; x++)
+		result[s1_len++] = s2[x];
+
+	result[s1_len] = '\0';
 	return (result);
 }
