33ce5a1504c525259b8700b114c362bd51d4b79c
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-04-18 20:28:30 +0300

    2-print_dog correct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,13 +9,16 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name)
-		printf("Name: %s\n", d->name);
-	else
-		printf("Name: (nil)\n");
-	printf("Age: %f\n", d->age);
-	if (d->owner)
+	if (d)
+	{
+		if (d->name)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->owner)
 		printf("Owner: %s\n", d->owner);
-	else
-		printf("Owner: (nil)\n");
+		else
+			printf("Owner: (nil)\n");
+	}
 }
