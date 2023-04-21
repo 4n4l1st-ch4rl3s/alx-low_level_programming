2f25db49908047169683d9c59e42f5a5fcb7a118
4n4l1st-ch4rl3s <63286909+4n4l1st-ch4rl3s@users.noreply.github.com>
2023-04-21 08:40:04 +0300

    1-array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator -  Entry point
+ * @array: Pointer 1
+ * @size: size or array
+ * @action: funcion pointer
+ * @int: var type for function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if(!array || !action)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
